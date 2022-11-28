20261829685ef1cbdee1ed6c862090d20ac2cb7f
furahaderick <yvesderricks@gmail.com>
2022-11-28 10:26:03 +0200

    Basic UI

---
+++ app.js
@@ -0,0 +1,7 @@
+let fortunes = [
+   "You will meet someone new today",
+   "You will go to someplace soon",
+   "You will eat a new dish todaye",
+];
+
+let fortune = document.getElementById("fortune");
