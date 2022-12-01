ea906de9710c51933aa19445d9bf73604b049360
furahaderick <yvesderricks@gmail.com>
2022-12-01 13:59:04 +0200

    Added logic

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 let fortunes = [
    "You will meet someone new today",
    "You will go to someplace soon",
-   "You will eat a new dish todaye",
+   "You will eat a new dish today",
 ];
 
-let fortune = document.getElementById("fortune");
+let loading = [
+   "gazing into the mysteries of the universe...",
+   "looking into your future...",
+]
+
+let fortunebox = document.getElementById("fortune");
+
+function getFortune() {
+   let index = Math.floor(Math.random() * (fortunes.length));
+   let loadIndex = Math.floor(Math.random() * (loading.length));
+   let load = loading[loadIndex];
+   let fortune = fortunes[index];
+
+   fortunebox.innerHTML = load;
+
+   function change() {
+      fortunebox.innerHTML = fortune; 
+   }
+    setTimeout(change, 2000);
+}
